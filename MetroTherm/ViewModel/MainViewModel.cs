2c67d2c572b1e21adb8dc436349d66a5ffafda85
asbj1477 <asbj1477@gmail.com>
2025-11-18 17:27:53 +0100

    bedre datahandler

--- MetroTherm/ViewModel/MainViewModel.cs
+++ MetroTherm/ViewModel/MainViewModel.cs
@@ -7,28 +7,40 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MetroTherm.ViewModel 
 { 
 public class MainViewModel : INotifyPropertyChanged
 {
-    public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-    ObservableCollection<Customer> Customers = new ObservableCollection<Customer>();
-    ObservableCollection<Equipment> Equipments = new ObservableCollection<Equipment>();     
-        // ingen constructor-logik = ingen hårdkodet data
+        ObservableCollection<Customer> Customers = new ObservableCollection<Customer>();
+        ObservableCollection<Equipment> Equipments = new ObservableCollection<Equipment>();
+
         public MainViewModel()
         {
-            IDataHandler dataHandler = new DataHandler("CustomerData.tsv"); 
-            dataHandler.LoadData(true); // Load customer data   
-            Customers = dataHandler.GetCustomerOverview();
+            IDataHandler dataHandler = new DataHandler("kundeliste.txt");
+            Customers = new ObservableCollection<Customer>(dataHandler.LoadData<Customer>());
 
-            IDataHandler equipmentDataHandler = new DataHandler("EquipmentData.tsv");   
-            equipmentDataHandler.LoadData(false);    
-            Equipments = equipmentDataHandler.GetEquipmentOverview();
+            IDataHandler equipmentDataHandler = new DataHandler("myuplink_points_file1_LSC-HL000209-RXpO4.txt");
+            Equipments = new ObservableCollection<Equipment>(equipmentDataHandler.LoadData<Equipment>());
+            
+            foreach (var item in Equipments) MessageBox.Show(item.ParameterName);
+            foreach (var item in Customers) MessageBox.Show(item.Name);
+        }
+
+        public void showEquipment(Equipment equipment)
+        {
+            
+        }
+
+        public Equipment chooseEquipment(Equipment equipment) 
+        { 
+                return equipment;
         }
    }
 }
