72cc77db024dd28ee399c60ef4d117daf86a635f
Niklas Frank <nf@veflinge-station.dk>
2025-11-18 10:30:03 +0100

    Lille UI update

--- MetroTherm/MainWindow.xaml.cs
+++ MetroTherm/MainWindow.xaml.cs
@@ -25,59 +25,7 @@ namespace MetroTherm
             this.DataContext = new MainViewModel();
 
         }
-
-        //private void AddEquipment_Click(object sender, RoutedEventArgs e)
-        //{
-        //    //// Hent ViewModel
-        //    //if (DataContext is MainViewModel vm)
-        //    //{
-        //    //    // Lav nyt Equipment baseret på UI-felterne
-        //    //    var eq = new Equipment
-        //    //    {
-        //    //        DeviceId = TxtDeviceId.Text,
-        //    //        ProductName = TxtProductName.Text,
-        //    //        Model = TxtModel.Text,
-        //    //        ConnectionState = ChkConnected.IsChecked == true
-        //        };
-
-        //        // Tilføj til listen – DataGrid opdaterer sig automatisk
-        //        vm.Equipments.Add(eq);
-
-        //        // (valgfrit) ryd felter
-        //        //TxtDeviceId.Clear();
-        //        //TxtProductName.Clear();
-        //        //TxtModel.Clear();
-        //        //ChkConnected.IsChecked = false;
-        //    }
-        //}
-        //private void AddCustomer_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if (DataContext is MainViewModel vm)
-        //    {
-        //        var cust = new Customer
-        //        {
-        //            CustomerId = TxtCustomerId.Text,
-        //            Name = TxtCustomerName.Text,
-        //            CustomerAddress = TxtCustomerAddress.Text,
-        //            //PhoneNumber = TxtCustomerPhone.Text,
-        //            //Email = TxtCustomerEmail.Text
-        //        };
-
-        //        vm.Customers.Add(cust);
-
-        //        // (valgfrit) ryd felter
-        //        TxtCustomerId.Clear();
-        //        TxtCustomerName.Clear();
-        //        TxtCustomerAddress.Clear();
-        //        TxtCustomerPhone.Clear();
-        //        TxtCustomerEmail.Clear();
-        //    }
-        //}
-
-
-
-
     }
 }
 
-//}
+
