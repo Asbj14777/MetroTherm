4f16ecb8ec1a211e3a74c81af1aa000657134c7a
AsiyeDikenn <asiyediken@icloud.com>
2025-11-19 19:29:50 +0100

    Repositoryklasser klar til at blive godkendt af teammedlemmer

--- MetroTherm/Models/CustomerRepository.cs
+++ MetroTherm/Models/CustomerRepository.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MetroTherm.Models
 {
-    class CustomerRepository
+    public class CustomerRepository
     {
+        private readonly List<Customer> _customers = new(); // gemmer kunder i hukommelsen
+
+        public CustomerRepository()     
+        {          
+        }
+
+        // Tilføj en kunde
+        public void AddCustomer(Customer customer)
+        {
+            if (customer == null) throw new ArgumentNullException(nameof(customer)); 
+            _customers.Add(customer);
+        }
+
+        // Slet en kunde
+        public void DeleteCustomer(Customer customer)
+        {
+            if (customer == null) return;
+            var existing = _customers.FirstOrDefault(c => c.ID == customer.ID); 
+            if (existing != null)
+            {
+                _customers.Remove(existing);
+            }
+           
+        }
+
+        // Opdater en kunde
+        public void UpdateCustomer(Customer customer)
+        {
+            if (customer == null) throw new ArgumentNullException(nameof(customer));
+            var index = _customers.FindIndex(c => c.ID == customer.ID); // Find kundens indeks
+            if (index >= 0) // Hvis kunden findes, opdater den
+            {
+                _customers[index] = customer;
+            }
+        }
+
+        // Hent alle kunder
+        public List<Customer> GetAll()
+        {
+            return new List<Customer>(_customers);
+        }
     }
 }
