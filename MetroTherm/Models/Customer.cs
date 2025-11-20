9b9e7c54015022be243ee122e6a7fee1b0c75c12
Asbj14777 <andersenasbjornmeyer@gmail.com>
2025-11-20 12:45:16 +0100

    Refactor Customer class to simplify properties
    Refactored Customer class to use auto-implemented properties and removed INotifyPropertyChanged implementation.

--- MetroTherm/Models/Customer.cs
+++ MetroTherm/Models/Customer.cs
@@ -1,72 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace MetroTherm.Models
 {
-    public class Customer : INotifyPropertyChanged
+    public class Customer
     {
-        private string _id; 
-        private string _name;   
-        private string _addresse;   
-
-        public string ID
-        {
-            get { return _id; }
-            set
-            {
-                if (_id != value)
-                {
-                    _id = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
-
-        public string Name
-        {
-            get { return _name; }
-            set
-            {
-                if (_name != value)
-                {
-                    _name = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }   
-
-
-        public string Address
-        {
-            get { return _addresse; }
-            set
-            {
-                if (_addresse != value)
-                {
-                    _addresse = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }       
-
-
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
 
         public Customer(string id, string name, string address)
         {
-            _id = id;
-            _name = name;
-            _addresse = address;
-        }       
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        private void NotifyPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            ID = id;
+            Name = name;
+            Address = address;
         }
+
+        public override string ToString() => $"ID : {ID}, Name: {Name}, Address: {Address}"; 
     }
 }
