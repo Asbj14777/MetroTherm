8685922a701867b2fa9397b90b01eb51f5446a89
Niklas Frank <nf@veflinge-station.dk>
2025-11-23 15:01:09 +0100

    Ændret modelnavn, gjort at combox fungere og bundet korrekt samt at elpris tekstbokse ændre sig til kundes modeller

--- MetroTherm/ViewModel/MainViewModel.cs
+++ MetroTherm/ViewModel/MainViewModel.cs
@@ -69,13 +69,6 @@ namespace MetroTherm.ViewModel
             return equipment;
         }
 
-        private Customer _billingCustomer;
-        public Customer BillingCustomer
-        {
-            get => _billingCustomer;
-            set { _billingCustomer = value; OnPropertyChanged(); }
-        }
-
         private Equipment _billingEquipment;
         public Equipment BillingEquipment
         {
@@ -102,7 +95,94 @@ namespace MetroTherm.ViewModel
             get => _pricePerKwh3;
             set { _pricePerKwh3 = value; OnPropertyChanged(); }
         }
+        private Customer _billingCustomer;
+        public Customer BillingCustomer
+        {
+            get => _billingCustomer;
+            set
+            {
+                if (_billingCustomer != value)
+                {
+                    _billingCustomer = value;
+                    OnPropertyChanged();
+                    UpdateBillingEquipments();
+                }
+            }
+        }
 
+        private ObservableCollection<Equipment> _billingEquipments = new ObservableCollection<Equipment>();
+        public ObservableCollection<Equipment> BillingEquipments
+        {
+            get => _billingEquipments;
+            set { _billingEquipments = value; OnPropertyChanged(); }
+        }
+
+        public void UpdateBillingEquipments()
+        {
+            if (BillingCustomer == null)
+            {
+                BillingEquipments = new ObservableCollection<Equipment>();
+                return;
+            }
+
+            string name = BillingCustomer.Name?.Trim();
+
+            string mustContain = name switch
+            {
+                "Lene Mortensen" => "RXpO4",
+                "Anders Poulsen" => "gK6s8",
+                _ => null
+            };
+
+            IEnumerable<Equipment> filtered;
+
+            if (!string.IsNullOrEmpty(mustContain))
+            {
+                filtered = Equipments.Where(e =>
+                !string.IsNullOrEmpty(e.DeviceId) &&
+                e.DeviceId.Contains(mustContain, StringComparison.OrdinalIgnoreCase)).GroupBy(e => e.ParameterName).Select(g => g.First());           
+
+            }
+            else
+            {
+                filtered = Enumerable.Empty<Equipment>();
+            }
+
+            BillingEquipments = new ObservableCollection<Equipment>(filtered);
+
+            
+            var models = BillingEquipments
+                .Select(e => e.ParameterName)
+                .Take(3)
+                .ToList();
+
+            ModelName1 = models.Count > 0 ? models[0] : "";
+            ModelName2 = models.Count > 1 ? models[1] : "";
+            ModelName3 = models.Count > 2 ? models[2] : "";
+
+
+        }
+        private string _modelName1;
+        public string ModelName1
+        {
+            get => _modelName1;
+            set { _modelName1 = value; OnPropertyChanged(); }
+        }
+
+        private string _modelName2;
+        public string ModelName2
+        {
+            get => _modelName2;
+            set { _modelName2 = value; OnPropertyChanged(); }
+        }
+
+        private string _modelName3;
+        public string ModelName3
+        {
+            get => _modelName3;
+            set { _modelName3 = value; OnPropertyChanged(); }
+        }
 
     }
 }
+
